b22b5a17ee87dfd9160633cea5074bc1e1e504f3
Prabin Bastakoti <Prabinbastakoti1@gmail.com>
2022-09-02 12:41:41 +0545

    Updated default profile picture in profile page

--- lmsproject/models.py
+++ lmsproject/models.py
@@ -10,7 +10,7 @@ class User(db.Model, UserMixin):
     firstname = db.Column(db.String(20), nullable= False)
     lastname = db.Column(db.String(20), nullable= False)
     email = db.Column(db.String(100),unique=True,nullable=False)
-    image_file = db.Column(db.String(20),nullable=False,default= 'default.jpg')
+    image_file = db.Column(db.String(20),nullable=False,default= 'user.png')
     phonenumber = db.Column(db.String(20),unique=True,nullable=False)
     password = db.Column(db.String(60),nullable= False)
     birthdate = db.Column(db.String(20),nullable=False)
